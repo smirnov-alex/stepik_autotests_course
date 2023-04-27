069a5d8c4e5cccebc92fc7a9117498055c6fb86b
Alex <smirnov1991alexx@gmail.com>
2023-04-27 10:51:14 +0500

    set correct link

--- module3_task/conftest.py
+++ module3_task/conftest.py
@@ -5,29 +5,16 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default='chrome',
-                     help='Choose browser: chrome or firefox')
-    parser.addoption('--language', action='store', default='ru',
-                     help='Choose language')
+    parser.addoption('--language', action='store', help='Choose language')
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption('browser_name')
     user_language = request.config.getoption('language')
-    browser = None
-    if browser_name == 'chrome':
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome(options=options)
-        print("\nstart chrome browser for test..")
-    elif browser_name == 'firefox':
-        print("\nstart firefox browser for test..")
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(firefox_profile=fp)
-    else:
-        raise pytest.UsageError('--browser_name should be chrome or firefox')
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    print("\nstart chrome browser for test..")
     yield browser
     print("\nquit browser..")
     browser.quit()
--- module3_task/test_items.py
+++ module3_task/test_items.py
@@ -1,10 +1,11 @@
 from selenium.webdriver.common.by import By
 import time
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_guest_should_see_login_link_fail(browser):
     browser.get(link)
+    time.sleep(15)
     browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
 
